55c398efe7b87a4ccbafaad0f9d4c3960766d6f5
Xiang Wang <1846468966@qq.com>
2016-03-19 11:13:16 +0800

    add the requirements

--- README.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Xiang Wang @ 2016-03-18 18:00:12
-
-git clone git@github.com:ramwin1/crawler_youku.git
-
--- crawler.py
+++ crawler.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Xiang Wang @ 2016-03-18 18:00:46
 
